1c1151ba0dadba93a44d26331be80eeda91ca3f2
lexcibien <alexci@hotmail.com>
2026-03-04 00:27:13 -0300

    multiple menus

--- src/tests/main_tft.cpp
+++ src/tests/main_tft.cpp
@@ -1,216 +1,253 @@
-// This example draws an animated dial with a rotating needle.
-
-// The dial is a jpeg image, the needle is created using a rotated
-// Sprite. The example operates by reading blocks of pixels from the
-// TFT, thus the TFT setup must support reading from the TFT CGRAM.
-
-// The sketch operates by creating a copy of the screen block where
-// the needle will be drawn, the needle is then drawn on the screen.
-// When the needle moves, the original copy of the screen area is
-// pushed to the screen to over-write the needle graphic. A copy
-// of the screen where the new position will be drawn is then made
-// before drawing the needle in the new position. This technique
-// allows the needle to move over other screen graphics.
-
-// The sketch calculates the size of the buffer memory required and
-// reserves the memory for the TFT block copy.
-
-// Created by Bodmer 17/3/20 as an example to the TFT_eSPI library:
-// https://github.com/Bodmer/TFT_eSPI
-
-
-#include <Arduino.h>
-
-#define NEEDLE_LENGTH 35  // Visible length
-#define NEEDLE_WIDTH   5  // Width of needle - make it an odd number
-#define NEEDLE_RADIUS 90  // Radius at tip
-#define NEEDLE_COLOR1 TFT_MAROON  // Needle periphery colour
-#define NEEDLE_COLOR2 TFT_RED     // Needle centre colour
-#define DIAL_CENTRE_X 120
-#define DIAL_CENTRE_Y 120
-
-// Font attached to this sketch
-#include "NotoSansBold36.h"
-#define AA_FONT_LARGE NotoSansBold36
-
 #include <TFT_eSPI.h>
+#include "gauge1.h"
+#include "gauge2.h"
+#include "gauge3.h"
+#include "gauge4.h"
+#include "gauge5.h"
+#include "gauge6.h"
+#include "font.h"
+
+enum CONSOLE_STATE {
+  STATE_ROOT_MENU
+};
+char cmdBuffer[80];
+int ptrBuffer;
+int state;
+
 TFT_eSPI tft = TFT_eSPI();
-TFT_eSprite needle = TFT_eSprite(&tft); // Sprite object for needle
-TFT_eSprite spr = TFT_eSprite(&tft); // Sprite for meter reading
+TFT_eSprite img = TFT_eSprite(&tft);
+TFT_eSprite ln = TFT_eSprite(&tft);
 
-// Jpeg image array attached to this sketch
-#include "dial.h"
+double rad = 0.01745;
+int angle;
 
-// Include the jpeg decoder library
-#include <TJpg_Decoder.h>
+int sx = 120;
+int sy = 120;
+int r = 76;
 
-uint16_t* tft_buffer;
-bool      buffer_loaded = false;
-uint16_t  spr_width = 0;
-uint16_t  bg_color = 0;
-// =======================================================================================
-// This function will be called during decoding of the jpeg file
-// =======================================================================================
-bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
-  // Stop further decoding as image is running off bottom of screen
-  if (y >= tft.height()) return 0;
+int32_t x[360];
+int32_t y[360];
+int32_t x2[360];
+int32_t y2[360];
 
-  // This function will clip the image block rendering automatically at the TFT boundaries
-  tft.pushImage(x, y, w, h, bitmap);
+const int pwmFreq = 5000;
+const int pwmResolution = 8;
+const int pwmLedChannelTFT = 0;
 
-  // Return 1 to decode next block
-  return 1;
-}
+int chosenOne = 0;
+int minValue[6] = { 0,20,0,0,0,80 };
+int maxValue[6] = { 40,100,60,80,70,160 };
+int dbounce = 0;
 
-void createNeedle();
-void plotNeedle(int16_t angle, uint16_t ms_delay);
+void rcvCharacter(uint8_t chr);
+void handleConsoleCmd();
+void handleShortCmd();
+void showHelp();
+void handleConfigCmd();
 
-// =======================================================================================
-// Setup
-// =======================================================================================
 void setup() {
-  Serial.begin(115200); // Debug only
+  ptrBuffer = 0;
+  state = STATE_ROOT_MENU;
+  // ledcSetup(pwmLedChannelTFT, pwmFreq, pwmResolution);
+  // ledcAttachPin(5, pwmLedChannelTFT);
+  // ledcWrite(pwmLedChannelTFT, 90);
 
-  // The byte order can be swapped (set true for TFT_eSPI)
-  TJpgDec.setSwapBytes(true);
+  pinMode(12, INPUT_PULLUP);
 
-  // The jpeg decoder must be given the exact name of the rendering function above
-  TJpgDec.setCallback(tft_output);
-
-  tft.begin();
+  tft.init();
   tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
+  tft.setSwapBytes(true);
+  img.setSwapBytes(true);
+  tft.fillScreen(TFT_ORANGE);
+  img.createSprite(240, 240);
 
-  // Draw the dial
-  TJpgDec.drawJpg(0, 0, dial, sizeof(dial));
-  tft.drawCircle(DIAL_CENTRE_X, DIAL_CENTRE_Y, NEEDLE_RADIUS - NEEDLE_LENGTH, TFT_DARKGREY);
 
-  // Load the font and create the Sprite for reporting the value
-  spr.loadFont(AA_FONT_LARGE);
-  spr_width = spr.textWidth("777"); // 7 is widest numeral in this font
-  spr.createSprite(spr_width, spr.fontHeight());
-  bg_color = tft.readPixel(120, 120); // Get colour from dial centre
-  spr.fillSprite(bg_color);
-  spr.setTextColor(TFT_WHITE, bg_color, true);
-  spr.setTextDatum(MC_DATUM);
-  spr.setTextPadding(spr_width);
-  spr.drawNumber(0, spr_width / 2, spr.fontHeight() / 2);
-  spr.pushSprite(DIAL_CENTRE_X - spr_width / 2, DIAL_CENTRE_Y - spr.fontHeight() / 2);
+  tft.setPivot(60, 60);
+  img.setTextDatum(4);
+  img.setTextColor(TFT_BLACK, 0xAD55);
+  img.setFreeFont(&Orbitron_Medium_28);
 
-  // Plot the label text
-  tft.setTextColor(TFT_WHITE, bg_color);
-  tft.setTextDatum(MC_DATUM);
-  tft.drawString("(degrees)", DIAL_CENTRE_X, DIAL_CENTRE_Y + 48, 2);
+  int i = 0;
+  int a = 136;
 
-  // Define where the needle pivot point is on the TFT before
-  // creating the needle so boundary calculation is correct
-  tft.setPivot(DIAL_CENTRE_X, DIAL_CENTRE_Y);
-
-  // Create the needle Sprite
-  createNeedle();
-
-  // Reset needle position to 0
-  plotNeedle(0, 0);
-
-  delay(2000);
-}
-
-// =======================================================================================
-// Loop
-// =======================================================================================
-void loop() {
-  uint16_t angle = random(241); // random speed in range 0 to 240
-
-  // Plot needle at random angle in range 0 to 240, speed 40ms per increment
-  plotNeedle(angle, 30);
-
-  // Pause at new position
-  delay(2500);
-}
-
-// =======================================================================================
-// Create the needle Sprite
-// =======================================================================================
-void createNeedle(void) {
-  needle.setColorDepth(16);
-  needle.createSprite(NEEDLE_WIDTH, NEEDLE_LENGTH);  // create the needle Sprite
-
-  needle.fillSprite(TFT_BLACK); // Fill with black
-
-  // Define needle pivot point relative to top left corner of Sprite
-  uint16_t piv_x = NEEDLE_WIDTH / 2; // pivot x in Sprite (middle)
-  uint16_t piv_y = NEEDLE_RADIUS;    // pivot y in Sprite
-  needle.setPivot(piv_x, piv_y);     // Set pivot point in this Sprite
-
-  // Draw the red needle in the Sprite
-  needle.fillRect(0, 0, NEEDLE_WIDTH, NEEDLE_LENGTH, TFT_MAROON);
-  needle.fillRect(1, 1, NEEDLE_WIDTH - 2, NEEDLE_LENGTH - 2, TFT_RED);
-
-  // Bounding box parameters to be populated
-  int16_t min_x;
-  int16_t min_y;
-  int16_t max_x;
-  int16_t max_y;
-
-  // Work out the worst case area that must be grabbed from the TFT,
-  // this is at a 45 degree rotation
-  needle.getRotatedBounds(45, &min_x, &min_y, &max_x, &max_y);
-
-  // Calculate the size and allocate the buffer for the grabbed TFT area
-  tft_buffer = (uint16_t*)malloc(((max_x - min_x) + 2) * ((max_y - min_y) + 2) * 2);
-}
-
-// =======================================================================================
-// Move the needle to a new position
-// =======================================================================================
-void plotNeedle(int16_t angle, uint16_t ms_delay) {
-  static int16_t old_angle = -120; // Starts at -120 degrees
-
-  // Bounding box parameters
-  static int16_t min_x;
-  static int16_t min_y;
-  static int16_t max_x;
-  static int16_t max_y;
-
-  if (angle < 0) angle = 0; // Limit angle to emulate needle end stops
-  if (angle > 240) angle = 240;
-
-  angle -= 120; // Starts at -120 degrees
-
-  // Move the needle until new angle reached
-  while (angle != old_angle || !buffer_loaded) {
-
-    if (old_angle < angle) old_angle++;
-    else old_angle--;
-
-    // Only plot needle at even values to improve plotting performance
-    if ((old_angle & 1) == 0) {
-      if (buffer_loaded) {
-        // Paste back the original needle free image area
-        tft.pushRect(min_x, min_y, 1 + max_x - min_x, 1 + max_y - min_y, tft_buffer);
-      }
-
-      if (needle.getRotatedBounds(old_angle, &min_x, &min_y, &max_x, &max_y)) {
-        // Grab a copy of the area before needle is drawn
-        tft.readRect(min_x, min_y, 1 + max_x - min_x, 1 + max_y - min_y, tft_buffer);
-        buffer_loaded = true;
-      }
-
-      // Draw the needle in the new position, black in needle image is transparent
-      needle.pushRotated(old_angle, TFT_BLACK);
-
-      // Wait before next update
-      delay(ms_delay);
-    }
-
-    // Update the number at the centre of the dial
-    spr.setTextColor(TFT_WHITE, bg_color, true);
-    spr.drawNumber(old_angle + 120, spr_width / 2, spr.fontHeight() / 2);
-    spr.pushSprite(120 - spr_width / 2, 120 - spr.fontHeight() / 2);
-
-    // Slow needle down slightly as it approaches the new position
-    if (abs(old_angle - angle) < 10) ms_delay += ms_delay / 5;
+  while (a != 44) {
+    x[i] = r * cos(rad * a) + sx;
+    y[i] = r * sin(rad * a) + sy;
+    x2[i] = (r - 20) * cos(rad * a) + sx;
+    y2[i] = (r - 20) * sin(rad * a) + sy;
+    i++;
+    a++;
+    if (a == 360)
+      a = 0;
   }
 }
 
-// =======================================================================================
+//min angle 136 or 137
+//max angle 43
+
+int a1, a2;
+int result = 0;
+int value = 0;
+int serialCnt;
+
+void loop() {
+  uint8_t in_byte;
+  serialCnt = 0;
+  while ((Serial.available() > 0) && serialCnt < 128) {
+    serialCnt++;
+    in_byte = Serial.read();
+    rcvCharacter(in_byte);
+  }
+  result = map(value, 0, 4095, minValue[chosenOne], maxValue[chosenOne]);
+  angle = map(result, minValue[chosenOne], maxValue[chosenOne], 0, 267);
+
+
+
+  if (chosenOne == 0)
+    img.pushImage(0, 0, 240, 240, gauge1);
+  if (chosenOne == 1)
+    img.pushImage(0, 0, 240, 240, gauge2);
+  if (chosenOne == 2)
+    img.pushImage(0, 0, 240, 240, gauge3);
+  if (chosenOne == 3)
+    img.pushImage(0, 0, 240, 240, gauge4);
+  if (chosenOne == 4)
+    img.pushImage(0, 0, 240, 240, gauge5);
+  if (chosenOne == 5)
+    img.pushImage(0, 0, 240, 240, gauge6);
+
+  if (chosenOne == 5)
+    img.drawFloat(result / 10.00, 2, 120, 114);
+  else if (chosenOne == 4)
+    img.drawString(String(result * 100), 120, 114);
+  else
+    img.drawString(String(result), 120, 114);
+  //img.drawString(String(analogRead(22)), 30,10,2);
+
+  a1 = angle - 4;
+  a2 = angle + 4;
+
+  if (a1 < 0)
+    a1 = angle - 4 + 359;
+  if (a2 >= 359)
+    a2 = angle + 4 - 359;
+
+  if (result <= minValue[chosenOne] + 4)
+    img.fillTriangle(x[angle], y[angle], x2[angle], y2[angle], x2[a2 + 2], y2[a2 + 2], TFT_RED);
+  else if (result >= maxValue[chosenOne] - 4)
+    img.fillTriangle(x[angle], y[angle], x2[a1 - 2], y2[a1 - 2], x2[angle], y2[angle], TFT_RED);
+  else
+    img.fillTriangle(x[angle], y[angle], x2[a1], y2[a1], x2[a2], y2[a2], TFT_RED);
+
+
+  img.pushSprite(0, 0);
+}
+
+void rcvCharacter(uint8_t chr) {
+  if (chr == 10 || chr == 13) { //command done. Parse it.
+    handleConsoleCmd();
+    ptrBuffer = 0; //reset line counter once the line has been processed
+  } else {
+    cmdBuffer[ptrBuffer++] = (unsigned char)chr;
+    if (ptrBuffer > 79)
+      ptrBuffer = 79;
+  }
+}
+
+void handleConsoleCmd() {
+  if (state == STATE_ROOT_MENU) {
+    if (ptrBuffer == 1) {
+      //command is a single ascii character
+      handleShortCmd();
+    } else { //at least two bytes
+      boolean equalSign = false;
+      for (int i = 0; i < ptrBuffer; i++) if (cmdBuffer[i] == '=') equalSign = true;
+      cmdBuffer[ptrBuffer] = 0; //make sure to null terminate
+      if (equalSign) handleConfigCmd();
+    }
+    ptrBuffer = 0; //reset line counter once the line has been processed
+  }
+}
+
+void handleShortCmd() {
+  uint8_t val;
+
+  switch (cmdBuffer[0]) {
+    //non-lawicel commands
+    case 'h':
+    case '?':
+    case 'H':
+    default:
+      showHelp();
+      break;
+  }
+}
+
+void handleConfigCmd() {
+  int i;
+  int newValue;
+  char* newString;
+  bool writeEEPROM = false;
+  bool writeDigEE = false;
+  char* dataTok;
+
+  //Logger::debug("Cmd size: %i", ptrBuffer);
+  if (ptrBuffer < 6)
+    return; //4 digit command, =, value is at least 6 characters
+  cmdBuffer[ptrBuffer] = 0; //make sure to null terminate
+  String cmdString = String();
+  unsigned char whichEntry = '0';
+  i = 0;
+
+  while (cmdBuffer[i] != '=' && i < ptrBuffer) {
+    cmdString.concat(String(cmdBuffer[i++]));
+  }
+  i++; //skip the =
+  if (i >= ptrBuffer) {
+    Serial.printf("Command needs a value..ie TORQ=3000");
+    Serial.printf("");
+    return; //or, we could use this to display the parameter instead of setting
+  }
+
+  // strtol() is able to parse also hex values (e.g. a string "0xCAFE"), useful for enable/disable by device id
+  newValue = strtol((char*)(cmdBuffer + i), NULL, 0); //try to turn the string into a number
+  newString = (char*)(cmdBuffer + i); //leave it as a string
+
+  cmdString.toUpperCase();
+
+  if (cmdString.startsWith("VALUE")) {
+    if (newValue >= 0 && newValue <= 4095) {
+      Serial.printf("Setting CAN0 Nominal Speed to %i", newValue);
+      value = newValue;
+
+      writeEEPROM = true;
+    } else Serial.printf("Invalid baud rate! Enter a value 32000 - 1000000");
+  } else if (cmdString == String("MENU")) {
+    if (newValue >= 0 && newValue <= 6) {
+      Serial.printf("Setting CAN0 Nominal Speed to %i", newValue);
+      chosenOne = newValue;
+
+      writeEEPROM = true;
+    } else Serial.printf("Invalid baud rate! Enter a value 1 - 6");
+  } else {
+    Serial.printf("Unknown command");
+  }
+}
+
+void showHelp() {
+  //Show build # here as well in case people are using the native port and don't get to see the start up messages
+  Serial.println("System Menu:");
+  Serial.println();
+  Serial.println("Enable line endings of some sort (LF, CR, CRLF)");
+  Serial.println();
+  Serial.println("Short Commands:");
+  Serial.println("h = help (displays this message)");
+  Serial.println();
+  Serial.println("Config Commands (enter command=newvalue). Current values shown in parenthesis:");
+  Serial.println();
+
+  Serial.printf("MENU=%i - Escolha entre os menus (0=coolant pressure, 1=coolant temp)", chosenOne);
+  Serial.println();
+  Serial.printf("VALUE=%i - set log level (0-4095)", value);
+  Serial.println();
+}
